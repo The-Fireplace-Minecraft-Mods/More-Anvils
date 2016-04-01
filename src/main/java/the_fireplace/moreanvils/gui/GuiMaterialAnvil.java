64589f0fb0c0acc348dbdcc6a4407ee0bfb3a2c6
The_Fireplace <the.f1repl4ce@gmail.com>
2016-03-31 21:30:55 -0500

    Formatting, diamond and gold hammers for the diamond and gold anvils

--- src/main/java/the_fireplace/moreanvils/gui/GuiMaterialAnvil.java
+++ src/main/java/the_fireplace/moreanvils/gui/GuiMaterialAnvil.java
@@ -11,6 +11,7 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ICrafting;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.client.CPacketCustomPayload;
@@ -26,26 +27,25 @@ import java.io.IOException;
 import java.util.List;
 
 @SideOnly(Side.CLIENT)
-public class GuiMaterialAnvil extends GuiContainer implements ICrafting
-{
+public class GuiMaterialAnvil extends GuiContainer implements ICrafting {
     private static final ResourceLocation anvilResource = new ResourceLocation("textures/gui/container/anvil.png");
+    private static final ResourceLocation goldHammerResource = new ResourceLocation("moreanvils:textures/gui/gold_hammer.png");
+    private static final ResourceLocation diamondHammerResource = new ResourceLocation("moreanvils:textures/gui/diamond_hammer.png");
     private ContainerMaterialAnvil anvil;
     private GuiTextField nameField;
     private InventoryPlayer playerInventory;
 
-    public GuiMaterialAnvil(InventoryPlayer inventoryIn, World worldIn, MaterialAnvil anvil)
-    {
+    public GuiMaterialAnvil(InventoryPlayer inventoryIn, World worldIn, MaterialAnvil anvil) {
         super(new ContainerMaterialAnvil(inventoryIn, worldIn, Minecraft.getMinecraft().thePlayer, anvil));
         this.playerInventory = inventoryIn;
-        this.anvil = (ContainerMaterialAnvil)this.inventorySlots;
+        this.anvil = (ContainerMaterialAnvil) this.inventorySlots;
     }
 
     /**
      * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
      * window resizes, the buttonList is cleared beforehand.
      */
-    public void initGui()
-    {
+    public void initGui() {
         super.initGui();
         Keyboard.enableRepeatEvents(true);
         int i = (this.width - this.xSize) / 2;
@@ -62,8 +62,7 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
     /**
      * Called when the screen is unloaded. Used to disable keyboard repeat events
      */
-    public void onGuiClosed()
-    {
+    public void onGuiClosed() {
         super.onGuiClosed();
         Keyboard.enableRepeatEvents(false);
         this.inventorySlots.removeCraftingFromCrafters(this);
@@ -75,45 +74,34 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
      * @param mouseX Mouse x coordinate
      * @param mouseY Mouse y coordinate
      */
-    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         GlStateManager.disableLighting();
         GlStateManager.disableBlend();
         this.fontRendererObj.drawString(I18n.format("container.repair"), 60, 6, 4210752);
 
-        if (this.anvil.maximumCost > 0)
-        {
+        if (this.anvil.maximumCost > 0) {
             int i = 8453920;
             boolean flag = true;
             String s = I18n.format("container.repair.cost", this.anvil.maximumCost);
 
-            if (this.anvil.maximumCost >= this.anvil.maximumCap && !this.mc.thePlayer.capabilities.isCreativeMode)
-            {
+            if (this.anvil.maximumCost >= this.anvil.maximumCap && !this.mc.thePlayer.capabilities.isCreativeMode) {
                 s = I18n.format("container.repair.expensive");
                 i = 16736352;
-            }
-            else if (!this.anvil.getSlot(2).getHasStack())
-            {
+            } else if (!this.anvil.getSlot(2).getHasStack()) {
                 flag = false;
-            }
-            else if (!this.anvil.getSlot(2).canTakeStack(this.playerInventory.player))
-            {
+            } else if (!this.anvil.getSlot(2).canTakeStack(this.playerInventory.player)) {
                 i = 16736352;
             }
 
-            if (flag)
-            {
+            if (flag) {
                 int j = -16777216 | (i & 16579836) >> 2 | i & -16777216;
                 int k = this.xSize - 8 - this.fontRendererObj.getStringWidth(s);
                 int l = 67;
 
-                if (this.fontRendererObj.getUnicodeFlag())
-                {
+                if (this.fontRendererObj.getUnicodeFlag()) {
                     drawRect(k - 3, l - 2, this.xSize - 7, l + 10, -16777216);
                     drawRect(k - 2, l - 1, this.xSize - 8, l + 9, -12895429);
-                }
-                else
-                {
+                } else {
                     this.fontRendererObj.drawString(s, k, l + 1, j);
                     this.fontRendererObj.drawString(s, k + 1, l, j);
                     this.fontRendererObj.drawString(s, k + 1, l + 1, j);
@@ -130,25 +118,19 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
      * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
      * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
      */
-    protected void keyTyped(char typedChar, int keyCode) throws IOException
-    {
-        if (this.nameField.textboxKeyTyped(typedChar, keyCode))
-        {
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (this.nameField.textboxKeyTyped(typedChar, keyCode)) {
             this.renameItem();
-        }
-        else
-        {
+        } else {
             super.keyTyped(typedChar, keyCode);
         }
     }
 
-    private void renameItem()
-    {
+    private void renameItem() {
         String s = this.nameField.getText();
         Slot slot = this.anvil.getSlot(0);
 
-        if (slot != null && slot.getHasStack() && !slot.getStack().hasDisplayName() && s.equals(slot.getStack().getDisplayName()))
-        {
+        if (slot != null && slot.getHasStack() && !slot.getStack().hasDisplayName() && s.equals(slot.getStack().getDisplayName())) {
             s = "";
         }
 
@@ -159,8 +141,7 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
     /**
      * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
      */
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         super.mouseClicked(mouseX, mouseY, mouseButton);
         this.nameField.mouseClicked(mouseX, mouseY, mouseButton);
     }
@@ -168,12 +149,11 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
     /**
      * Draws the screen and all the components in it.
      *
-     * @param mouseX Mouse x coordinate
-     * @param mouseY Mouse y coordinate
+     * @param mouseX       Mouse x coordinate
+     * @param mouseY       Mouse y coordinate
      * @param partialTicks How far into the current tick (1/20th of a second) the game is
      */
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
         GlStateManager.disableLighting();
         GlStateManager.disableBlend();
@@ -184,12 +164,11 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
      * Draws the background layer of this container (behind the items).
      *
      * @param partialTicks How far into the current tick the game is, with 0.0 being the start of the tick and 1.0 being
-     * the end.
-     * @param mouseX Mouse x coordinate
-     * @param mouseY Mouse y coordinate
+     *                     the end.
+     * @param mouseX       Mouse x coordinate
+     * @param mouseY       Mouse y coordinate
      */
-    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(anvilResource);
         int i = (this.width - this.xSize) / 2;
@@ -197,17 +176,22 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
         this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
         this.drawTexturedModalRect(i + 59, j + 20, 0, this.ySize + (this.anvil.getSlot(0).getHasStack() ? 0 : 16), 110, 16);
 
-        if ((this.anvil.getSlot(0).getHasStack() || this.anvil.getSlot(1).getHasStack()) && !this.anvil.getSlot(2).getHasStack())
-        {
+        if ((this.anvil.getSlot(0).getHasStack() || this.anvil.getSlot(1).getHasStack()) && !this.anvil.getSlot(2).getHasStack()) {
             this.drawTexturedModalRect(i + 99, j + 45, this.xSize, 0, 28, 21);
+        }
+        if (this.anvil.material == ItemArmor.ArmorMaterial.DIAMOND) {
+            this.mc.getTextureManager().bindTexture(diamondHammerResource);
+            this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+        } else if (this.anvil.material == ItemArmor.ArmorMaterial.GOLD) {
+            this.mc.getTextureManager().bindTexture(goldHammerResource);
+            this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
         }
     }
 
     /**
      * update the crafting window inventory with the items in the list
      */
-    public void updateCraftingInventory(Container containerToSend, List<ItemStack> itemsList)
-    {
+    public void updateCraftingInventory(Container containerToSend, List<ItemStack> itemsList) {
         this.sendSlotContents(containerToSend, 0, containerToSend.getSlot(0).getStack());
     }
 
@@ -215,15 +199,12 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
      * Sends the contents of an inventory slot to the client-side Container. This doesn't have to match the actual
      * contents of that slot. Args: Container, slot number, slot contents
      */
-    public void sendSlotContents(Container containerToSend, int slotInd, ItemStack stack)
-    {
-        if (slotInd == 0)
-        {
+    public void sendSlotContents(Container containerToSend, int slotInd, ItemStack stack) {
+        if (slotInd == 0) {
             this.nameField.setText(stack == null ? "" : stack.getDisplayName());
             this.nameField.setEnabled(stack != null);
 
-            if (stack != null)
-            {
+            if (stack != null) {
                 this.renameItem();
             }
         }
@@ -234,11 +215,9 @@ public class GuiMaterialAnvil extends GuiContainer implements ICrafting
      * and enchanting level. Normally the first int identifies which variable to update, and the second contains the new
      * value. Both are truncated to shorts in non-local SMP.
      */
-    public void sendProgressBarUpdate(Container containerIn, int varToUpdate, int newValue)
-    {
+    public void sendProgressBarUpdate(Container containerIn, int varToUpdate, int newValue) {
     }
 
-    public void sendAllWindowProperties(Container containerIn, IInventory inventory)
-    {
+    public void sendAllWindowProperties(Container containerIn, IInventory inventory) {
     }
 }
